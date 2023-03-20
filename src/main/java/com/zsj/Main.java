8fc637b6615733b4dc1a681d2ab1a1e3e80a2c78
Zhanjia <Zhanjia@zsj.com>
2023-03-20 22:47:13 +0800

    second commit

--- src/main/java/com/zsj/Main.java
+++ src/main/java/com/zsj/Main.java
@@ -3,5 +3,6 @@ package com.zsj;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
